5208c314b919e3947177c8a5cfe5673f008f1322
narayanDivya <127798315+narayanDivya@users.noreply.github.com>
2023-03-26 21:13:31 -0400

    Extent reports (#10)
    Added code to generate pdf, html and spark extent reports

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 	@CucumberOptions(
-			plugin = {"pretty", "html:target\\bddtest1.html"}, // reporting purpose
+			plugin = {"pretty", "html:target\\cucumberReport.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, // reporting purpose
 			monochrome=false, //console output color
 			tags="", //tags from feature file
 			features = {"src\\test\\resources\\features"}, //location of feature files
